bf25ebca7176543e6e818c5586168d452197da27
Osamu Koga <github@osak.jp>
2023-08-24 01:30:25 +0900

    reformat code

--- main.js
+++ main.js
@@ -1,29 +1,29 @@
-Game.registerMod("amortization mod",{//this string needs to match the ID provided in your info.txt
-	init:function(){
+Game.registerMod("osak-cookie-clicker-mod", {
+	init: function () {
 		for (var i in Game.Objects) {
 			var building = Game.Objects[i];
 			var originalTooltip = building.tooltip;
-			building.tooltip = function() {
+			building.tooltip = function () {
 				let str = originalTooltip.bind(this).call();
 				if (this.locked || this.totalCookies == 0) {
 					return str;
 				}
 				let singleCps = this.storedTotalCps / this.amount * Game.globalCpsMult;
 				let amortize = this.price / singleCps;
-			
+
 				if (str.endsWith('</div>')) {
 					str = str.slice(0, -6);
-					str += '<div class="descriptionBlock"><b>'+ Beautify(amortize) + '</b> seconds to amortize the cost</div>';
+					str += '<div class="descriptionBlock"><b>' + Beautify(amortize) + '</b> seconds to amortize the cost</div>';
 					str += '</div>';
 				}
 				return str;
 			};
 		};
-		
+
 	},
-	save:function(){
+	save: function () {
 		return "";
 	},
-	load:function(str){
+	load: function (str) {
 	}
 });
